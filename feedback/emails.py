f004c221b9852010a0360492dbe79d625360d990
dgarzonmoreno <dgarzonmoreno@ufl.edu>
2020-07-22 21:44:48 -0400

    email worker succcessfully sends info, SMTP not set up but fuck it

--- feedback/emails.py
+++ feedback/emails.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 
 
 def send_feedback_email(email, message):
-    c = Context({'email': email, 'message': message})
+    c = ({'email': email, 'message': message})
 
     email_subject = render_to_string(
         'feedback/email/feedback_email_subject.txt', c).replace('\n', '')
@@ -13,7 +13,7 @@ def send_feedback_email(email, message):
 
     email = EmailMessage(
         email_subject, email_body, email,
-        [settings.DEFAULT_FROM_EMAIL], [],
+        ["dxnielgxrzon@gmail.com", "edwincarvajal008@gmail.com"], [],
         headers={'Reply-To': email}
     )
     return email.send(fail_silently=False)
